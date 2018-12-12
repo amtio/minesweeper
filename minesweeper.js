997a950965940cb7268ab806d45d4d97425a1cc8
amtio <45711455+amtio@users.noreply.github.com>
2018-12-12 12:15:57 +0100

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,14 @@
+// const printBoard = row => board[row].join(' | ');
+const printBoard = board => {
+  console.log('Current board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+const board=[[' ',' ',' '],
+             [' ',' ',' '],
+             [' ',' ',' ']];
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
